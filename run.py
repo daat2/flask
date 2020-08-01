353ec6259847366e43af09daa3718f6bf08502c8
daat2 <ibi.olu@gmail.com>
2020-08-01 00:05:39 +0000

    first commit

---
+++ run.py
@@ -0,0 +1,31 @@
+from flask import Flask, app
+import os
+from flask.templating import render_template
+ 
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+     return  render_template("index.html")
+@app.route("/about")
+def about():
+    return  render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return  render_template("contact.html")
+
+@app.route("/careers")
+def careers():
+    return  render_template("careers.html")
+
+
+
+
+if  __name__ == "__main__" :
+    app.run(host=os.environ.get("IP","0.0.0.0"),
+            port=int(os.environ.get("PORT","8000")),
+            debug=True)
+
